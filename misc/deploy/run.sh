37d22f7136066cc748d6ecb4f0bd7c86ff022135
Croydon <cr0ydon@outlook.com>
2017-06-19 13:10:02 +0200

    Add config initalization

--- misc/deploy/run.sh
+++ misc/deploy/run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ ! -f /notifico/config/config.py ]; then
+    cp /notifico/app/notifico/config.py /notifico/config/config.py
+fi
+
+if [ ! -f /notifico/config/database.db ]; then
+    python -m notifico init
+fi
+
 python -m notifico www --host 0.0.0.0 > /notifico/config/notifico-www.txt 2>&1 &
 python -m notifico bots > /notifico/config/notifico-bots.txt 2>&1 &
 
