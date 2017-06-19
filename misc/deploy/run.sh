aa640b710aa27e493d90c84fc40b8f9d5b83bb57
Croydon <cr0ydon@outlook.com>
2017-06-19 10:50:47 +0200

    Fix location of log files

--- misc/deploy/run.sh
+++ misc/deploy/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python -m notifico www --host 0.0.0.0 & > /notifico/notifico-www.txt
-python -m notifico bots & > /notifico/notifico-bots.txt
+python -m notifico www --host 0.0.0.0 & > /notifico/config/notifico-www.txt
+python -m notifico bots & > /notifico/config/notifico-bots.txt
