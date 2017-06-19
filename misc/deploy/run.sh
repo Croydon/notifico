4b99aec0828bf5eac1d15074aa8e1142a79956c1
Croydon <cr0ydon@outlook.com>
2017-06-19 12:35:15 +0200

    Try to fix log files

--- misc/deploy/run.sh
+++ misc/deploy/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python -m notifico www --host 0.0.0.0 & > /notifico/config/notifico-www.txt
-python -m notifico bots & > /notifico/config/notifico-bots.txt
+python -m notifico www --host 0.0.0.0 > /notifico/config/notifico-www.txt 2>&1 &
+python -m notifico bots > /notifico/config/notifico-bots.txt 2>&1 &
 
 tail -f /dev/null
